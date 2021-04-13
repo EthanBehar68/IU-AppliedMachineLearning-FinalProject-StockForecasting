155593e93ae76d81fcd41a3a5a049b1b13824ddc
rowlavel <rowlavel@iu.edu>
2021-04-13 18:39:46 -0400

    testing on silo

--- hmms/gaussian_hmm.py
+++ hmms/gaussian_hmm.py
@@ -1,3 +1,6 @@
+import sys
+from test import *
+from model import Model
 from fastquant import get_stock_data
 from hmmlearn import hmm
 import numpy as np
@@ -6,75 +9,30 @@ import matplotlib.pyplot as plt
 import math
 import time
 from multiprocessing import Pool
-import sys
 
-class Gaussian_HMM:
-    def __init__(self,n_components,algorithm,n_iter,d):
-        self.n_components = n_components
-        self.algorithm = algorithm
-        self.n_iter = n_iter
-        self.d = d
 
-        self.model = None
-    
-    def get_data(self, ticker, start_date, end_date):
-        return get_stock_data(ticker, start_date, end_date)
-
-    def data_prep(self, data):
-
-        df = pd.DataFrame(data=None, columns=['fracChange','fracHigh','fracLow'])
-        df['fracChange'] = (data['close']-data['open'])/data['open']
-        df['fracHigh'] = (data['high']-data['open'])/data['open']
-        df['fracLow'] = (data['open']-data['low'])/data['open']
-
-        return df
-
-    def plot_results(self, preds, actual, title):
-        fig, ax = plt.subplots(figsize=(15,5))
-        ax.set_title(title)
-        time = range(len(preds))
-        ax.plot(time,preds,color='tab:red',marker='s',markersize=2,linestyle='-',linewidth=1,label='forcast')
-        ax.plot(time,actual,color='tab:blue',marker='s',markersize=2,linestyle='-',linewidth=1,label='actual')
-        ax.set_xlabel('time')
-        ax.set_ylabel('stock price ($)')
-        ax.set_xticks(np.arange(0,len(preds)+10,10))
-        ax.set_xlim(0,len(preds)+10)
-        ax.xaxis.grid(True,ls='--')
-        ax.yaxis.grid(True,ls='--')
-        ax.legend()
-        plt.savefig(f'../imgs/{title}.png')
-    
-    def mean_abs_percent_error(self, y_pred, y_true):
-        return (1.0)/(len(y_pred))*((np.abs(y_pred-y_true)/np.abs(y_true))*100).sum()
+class GHMM(Model):
+    def __init__(self,params):
+        super().__init__(params)
+        self.n_components = params['n_components']
+        self.algorithm = params['algorithm']
+        self.n_iter = params['n_iter']
+        self.d = params['d']
     
     def train(self, train_data):
-        train_obs = self.data_prep(train_data)
+        self.train_obs = self.data_prep(train_data)
         self.model = hmm.GaussianHMM(n_components=self.n_components,
                                      algorithm=self.algorithm,
                                      n_iter=self.n_iter)
 
-        self.model.fit(train_obs)
-
-        return train_obs
+        self.model.fit(self.train_obs)
     
-    def log_lik_calc(self, observed, observations):
-        log_liks = []
-        for o in observations:
-            obs = np.vstack((observed,o))
-            log_lik = self.model.score(obs)
-            log_liks.append((o,log_lik))
-
-        return log_liks
-    
-    def test(self,test_data, train_obs):
-        
-        test_obs = self.data_prep(test_data)
-        
+    def predict(self,test_data):        
         test_close_prices = test_data['close'].values
         test_open_prices = test_data['open'].values
 
         # use a latency of d days. So observations start as training data
-        observed = train_obs.iloc[-self.d:].values
+        observed = self.train_obs.iloc[-self.d:].values
         
         # list to hold predictions
         preds = []
@@ -82,11 +40,9 @@ class Gaussian_HMM:
         # loop through all points we want to test
         for i in range(len(test_data)):
             # try 50x10x10 possible values for O_d+1
-            change = np.arange(-0.1,0.1,0.2/50)
-            high = np.arange(0,0.1,0.1/10)
-            low = np.arange(0,0.1,0.1/10)
+            change = np.arange(-0.2,0.2,0.4/5000)
 
-            observations = [np.array([c,h,l]) for l in low for h in high for c in change]
+            observations = [np.array([c]) for c in change]
             
             # compute all log likelihoods w/ their observations in parallel
             jump = int(len(observations)/20)
@@ -105,57 +61,40 @@ class Gaussian_HMM:
             observed = observed[1:]
 
             #calculate the close value from best
-            pred_close = best['obs'][0]*test_open_prices[i]+test_open_prices[i]
+            pred_close = best['obs']*test_open_prices[i]+test_open_prices[i]
             preds.append(pred_close)
 
-            print(f'{i+1}/{len(test_data)}',end='\r')
+            print(f'{i+1}/{len(test_data)}',end='\r',flush=True)
         print('DONE')
         return preds,test_close_prices
-            
+    
+    def log_lik_calc(self, observed, observations):
+        log_liks = []
+        for o in observations:
+            obs = np.vstack((observed,o))
+            log_lik = self.model.score(obs)
+            log_liks.append((o,log_lik))
+
+        return log_liks
+    
+    def data_prep(self, data):
+        df = pd.DataFrame(data=None, columns=['fracChange','fracHigh','fracLow'])
+        df['fracChange'] = (data['close']-data['open'])/data['open']
+
+        return df
+
 
 if __name__ == "__main__":
-    # training with apple feb-10-2003 -> sep-10-2004
-    # testing with apple sep-13-2004 -> jan-21-2005
+    params = {'n_components': 2, 
+              'algorithm': 'map', 
+              'n_iter': 100, 
+              'd': 5,
+              'name':'GHMM'}
     
-    model = Gaussian_HMM(n_components=5,
-                         algorithm="map",
-                         n_iter=100,
-                         d=5)
-    
-    train_data = model.get_data(ticker='AAPL',start_date='2003-02-10',end_date='2004-09-10')
-    test_data = model.get_data(ticker='AAPL',start_date='2004-09-13',end_date='2005-01-21')
-    
-    train_obs = model.train(train_data=train_data)
+    print('testing best found parameters paper tests')
+    test = Test(Model=GHMM, params=params, tests=paper_tests, f='ghmm-paper-tests.json', plot=True)
+    test.run_tests()
 
-    start = time.time()
-    preds,actual = model.test(test_data=test_data, train_obs=train_obs)
-    end = time.time()
-    print(f'model tested in {round((end-start)/60,2)} minutes')
-    error = model.mean_abs_percent_error(y_pred=preds, y_true=actual)
-    print(f'AAPL error: {error}')
-
-    model.plot_results(preds=preds, actual=actual, 
-                       title='Gaussian HMM AAPL forcasted vs actual stock prices Sep 2004 - Jan 2005')
-    
-    # training with IBM feb-10-2003 -> sep-10-2004
-    # testing with IBM sep-13-2004 -> jan-21-2005
-
-    model = Gaussian_HMM(n_components=5,
-                         algorithm="map",
-                         n_iter=100,
-                         d=5)
-    
-    train_data = model.get_data(ticker='IBM',start_date='2003-02-10',end_date='2004-09-10')
-    test_data = model.get_data(ticker='IBM',start_date='2004-09-13',end_date='2005-01-21')
-
-    train_obs = model.train(train_data=train_data)
-
-    start = time.time()
-    preds,actual = model.test(test_data=test_data, train_obs=train_obs)
-    end = time.time()
-    print(f'model tested in {round((end-start)/60,2)} minutes')
-    error = model.mean_abs_percent_error(y_pred=preds, y_true=actual)
-    print(f'IBM error: {error}')
-
-    model.plot_results(preds=preds, actual=actual, 
-                       title='Gaussian HMM IBM forcasted vs actual stock prices Sep 2004 - Jan 2005')
+    print('testing best found parameters own tests')
+    test = Test(Model=GHMM, params=params, tests=own_tests, f='ghmm-own-tests.json', plot=True)
+    test.run_tests()
