3588a4166b1ef06bc2b79349a8d10f3a4a0acf45
Ethan Behar <behar.ethan@gmail.com>
2021-04-22 18:54:14 -0400

    playground update

--- rnns/playground.py
+++ rnns/playground.py
@@ -2,13 +2,20 @@
 
 import quandl
 
-mydata = quandl.get("NSE/NIFTY_50", start_date='2011-01-01', end_date='2016-12-31')
 
-print(type(mydata))
+        #     {'ticker':'NSE/NIFTY_50', 'start':'2011-01-01', 'end':''},
+        # 'test':
+        #     {'ticker':'NSE/NIFTY_50', 'start':'2016-06-21', 'end':'2016-12-31'}
 
-mydata = mydata.drop(columns=['Turnover (Rs. Cr)'], axis=1).rename(columns={"Shares Traded": "Volume"})
-mydata = mydata.round(0)
-mydata = mydata.fillna(method='pad')
+mydata = quandl.get("NSE/NIFTY_50", start_date='2011-01-01', end_date='2016-04-20')
+print(mydata.shape)
+
+mydata = quandl.get("NSE/NIFTY_50", start_date='2016-04-21', end_date='2016-10-31')
+print(mydata.shape)
 
 
-print(mydata)
+# mydata = mydata.drop(columns=['Turnover (Rs. Cr)'], axis=1).rename(columns={"Shares Traded": "Volume"})
+# mydata = mydata.round(0)
+# mydata = mydata.fillna(method='pad')
+# mydata = mydata.dropna(axis=0)
+
