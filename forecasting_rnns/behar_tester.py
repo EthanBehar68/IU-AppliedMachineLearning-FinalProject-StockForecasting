fc46845bf7efad63e72375bf8e2b47834bf5c271
Ethan Behar <behar.ethan@gmail.com>
2021-04-26 22:11:08 -0400

    updating testing files for parallel processing

--- forecasting_rnns/behar_tester.py
+++ forecasting_rnns/behar_tester.py
@@ -5,128 +5,23 @@ if __name__ == "__main__":
 ####################
 # Behar Tests #
 ####################
-    # ['open', 'close'] Test
-    # Naming syntax please use
-    # {Paper}-{Std/Norm}-{Win/''}-{Discr/''}-{epoch}-{train columns}
-    params = {'lr': 0.001, # learning rate
-                'loss': 'mean_absolute_percentage_error', # Loss function
-                'activation': 'tanh', # Not used
-                'recurrent_activation': 'sigmoid', # Not used
-                'epochs': 250, #250/500
-                'batch_size': 150,
-                'd': 22, # Taken from Roonwidala et al.
-                'train_columns': ['open', 'close'],
-                'label_column': 'close', 
-                'name': 'Behar-Std-250-OpenClose', 
-                'discretization': False, # Use value rounding?
-                'fill_method': 'previous', # fillna method='pa'
-                'normalization': False, # Normalize or standardization?
-                'window_scaling': False } # Window scaling or bulk scaling?
-    
-    test = Test(Model=LSTM_Behar, params=params, tests=window_heavy_hitters_tests, f='', plot=True)
-    test.rolling_window_test('./forecasting_rnns/results/Behar-Std-250-OpenClose/')
-
-    # ['high', 'low', 'close'] Test
-    # Naming syntax please use
-    # {Paper}-{Std/Norm}-{Win/''}-{Discr/''}-{epoch}-{train columns}
-    params = {'lr': 0.001, # learning rate
-                'loss': 'mean_absolute_percentage_error', # Loss function
-                'activation': 'tanh', # Not used
-                'recurrent_activation': 'sigmoid', # Not used
-                'epochs': 250, #250/500
-                'batch_size': 150,
-                'd': 22, # Taken from Roonwidala et al.
-                'train_columns': ['high', 'low', 'close'],
-                'label_column': 'close', 
-                'name': 'Behar-Std-250-HighLowClose', 
-                'discretization': False, # Use value rounding?
-                'fill_method': 'previous', # fillna method='pa'
-                'normalization': False, # Normalize or standardization?
-                'window_scaling': False } # Window scaling or bulk scaling?
-    
-    test = Test(Model=LSTM_Behar, params=params, tests=window_heavy_hitters_tests, f='', plot=True)
-    test.rolling_window_test('./forecasting_rnns/results/Behar-Std-250-HighLowClose/')
-
     # ['high', 'low', 'open', 'close'] Test
     # Naming syntax please use
     # {Paper}-{Std/Norm}-{Win/''}-{Discr/''}-{epoch}-{train columns}
-    params = {'lr': 0.001, # learning rate
-                'loss': 'mean_absolute_percentage_error', # Loss function
-                'activation': 'tanh', # Not used
-                'recurrent_activation': 'sigmoid', # Not used
-                'epochs': 250, #250/500
+    params = {'lr': 0.001,
+                'loss': 'mean_absolute_percentage_error', 
+                'activation': 'tanh',
+                'recurrent_activation': 'sigmoid',
+                'epochs': 500,
                 'batch_size': 150,
-                'd': 22, # Taken from Roonwidala et al.
-                'train_columns': ['high', 'low', 'open', 'close'],
-                'label_column': 'close', 
-                'name': 'Behar-Std-250-HighLowOpenClose', 
-                'discretization': False, # Use value rounding?
-                'fill_method': 'previous', # fillna method='pa'
-                'normalization': False, # Normalize or standardization?
-                'window_scaling': False } # Window scaling or bulk scaling?
-    
-    test = Test(Model=LSTM_Behar, params=params, tests=window_heavy_hitters_tests, f='', plot=True)
-    test.rolling_window_test('./forecasting_rnns/results/Behar-Std-250-HighLowOpenClose/')
-
-    # ['open', 'close'] Test
-    # Naming syntax please use
-    # {Paper}-{Std/Norm}-{Win/''}-{Discr/''}-{epoch}-{train columns}
-    params = {'lr': 0.001, # learning rate
-                'loss': 'mean_absolute_percentage_error', # Loss function
-                'activation': 'tanh', # Not used
-                'recurrent_activation': 'sigmoid', # Not used
-                'epochs': 500, #250/500
-                'batch_size': 150,
-                'd': 22, # Taken from Roonwidala et al.
-                'train_columns': ['open', 'close'],
-                'label_column': 'close', 
-                'name': 'Behar-Std-500-OpenClose', 
-                'discretization': False, # Use value rounding?
-                'fill_method': 'previous', # fillna method='pa'
-                'normalization': False, # Normalize or standardization?
-                'window_scaling': False } # Window scaling or bulk scaling?
-    
-    test = Test(Model=LSTM_Behar, params=params, tests=window_heavy_hitters_tests, f='', plot=True)
-    test.rolling_window_test('./forecasting_rnns/results/Behar-Std-500-OpenClose/')
-
-    # ['high', 'low', 'close'] Test
-    # Naming syntax please use
-    # {Paper}-{Std/Norm}-{Win/''}-{Discr/''}-{epoch}-{train columns}
-    params = {'lr': 0.001, # learning rate
-                'loss': 'mean_absolute_percentage_error', # Loss function
-                'activation': 'tanh', # Not used
-                'recurrent_activation': 'sigmoid', # Not used
-                'epochs': 500, #250/500
-                'batch_size': 150,
-                'd': 22, # Taken from Roonwidala et al.
-                'train_columns': ['high', 'low', 'close'],
-                'label_column': 'close', 
-                'name': 'Behar-Std-500-HighLowClose', 
-                'discretization': False, # Use value rounding?
-                'fill_method': 'previous', # fillna method='pa'
-                'normalization': False, # Normalize or standardization?
-                'window_scaling': False } # Window scaling or bulk scaling?
-    
-    test = Test(Model=LSTM_Behar, params=params, tests=window_heavy_hitters_tests, f='', plot=True)
-    test.rolling_window_test('./forecasting_rnns/results/Behar-Std-500-HighLowClose/')
-
-    # ['high', 'low', 'open', 'close'] Test
-    # Naming syntax please use
-    # {Paper}-{Std/Norm}-{Win/''}-{Discr/''}-{epoch}-{train columns}
-    params = {'lr': 0.001, # learning rate
-                'loss': 'mean_absolute_percentage_error', # Loss function
-                'activation': 'tanh', # Not used
-                'recurrent_activation': 'sigmoid', # Not used
-                'epochs': 500, #250/500
-                'batch_size': 150,
-                'd': 22, # Taken from Roonwidala et al.
+                'd': 22,
                 'train_columns': ['high', 'low', 'open', 'close'],
                 'label_column': 'close', 
                 'name': 'Behar-Std-500-HighLowOpenClose', 
-                'discretization': False, # Use value rounding?
-                'fill_method': 'previous', # fillna method='pa'
-                'normalization': False, # Normalize or standardization?
-                'window_scaling': False } # Window scaling or bulk scaling?
+                'discretization': False,
+                'fill_method': 'previous',
+                'normalization': False,
+                'window_scaling': False }
     
     test = Test(Model=LSTM_Behar, params=params, tests=window_heavy_hitters_tests, f='', plot=True)
     test.rolling_window_test('./forecasting_rnns/results/Behar-Std-500-HighLowOpenClose/')
--- forecasting_rnns/pawar_tester.py
+++ forecasting_rnns/pawar_tester.py
@@ -4,129 +4,23 @@ from lstm_pawaretal import *
 if __name__ == "__main__":
 ####################
 # Pawar Tests #
-####################
-    # ['open', 'close'] Test
+    # ['close'] Test
     # Naming syntax please use
     # {Paper}-{Std/Norm}-{Win/''}-{Discr/''}-{epoch}-{train columns}
-    params = {'lr': 0.001, # learning rate
-                'loss': 'mean_absolute_percentage_error', # Loss function
-                'activation': 'tanh', # Not used
-                'recurrent_activation': 'sigmoid', # Not used
-                'epochs': 250, #250/500
+    params = {'lr': 0.001,
+                'loss': 'mean_absolute_percentage_error',
+                'activation': 'tanh',
+                'recurrent_activation': 'sigmoid',
+                'epochs': 100,
                 'batch_size': 150,
-                'd': 22, # Taken from Roonwidala et al.
-                'train_columns': ['open', 'close'],
+                'd': 22,
+                'train_columns': ['close'],
                 'label_column': 'close', 
-                'name': 'Pawar-Std-250-OpenClose', 
-                'discretization': False, # Use value rounding?
-                'fill_method': 'previous', # fillna method='pa'
-                'normalization': False, # Normalize or standardization?
-                'window_scaling': False } # Window scaling or bulk scaling?
+                'name': 'Pawar-Std-100-Close', 
+                'discretization': False,
+                'fill_method': 'previous',
+                'normalization': False,
+                'window_scaling': False }
     
     test = Test(Model=LSTM_PawarEtAl, params=params, tests=window_heavy_hitters_tests, f='', plot=True)
-    test.rolling_window_test('./forecasting_rnns/results/Pawar-Std-250-OpenClose/')
-
-    # ['high', 'low', 'close'] Test
-    # Naming syntax please use
-    # {Paper}-{Std/Norm}-{Win/''}-{Discr/''}-{epoch}-{train columns}
-    params = {'lr': 0.001, # learning rate
-                'loss': 'mean_absolute_percentage_error', # Loss function
-                'activation': 'tanh', # Not used
-                'recurrent_activation': 'sigmoid', # Not used
-                'epochs': 250, #250/500
-                'batch_size': 150,
-                'd': 22, # Taken from Roonwidala et al.
-                'train_columns': ['high', 'low', 'close'],
-                'label_column': 'close', 
-                'name': 'Pawar-Std-250-HighLowClose', 
-                'discretization': False, # Use value rounding?
-                'fill_method': 'previous', # fillna method='pa'
-                'normalization': False, # Normalize or standardization?
-                'window_scaling': False } # Window scaling or bulk scaling?
-    
-    test = Test(Model=LSTM_PawarEtAl, params=params, tests=window_heavy_hitters_tests, f='', plot=True)
-    test.rolling_window_test('./forecasting_rnns/results/Pawar-Std-250-HighLowClose/')
-
-    # ['high', 'low', 'open', 'close'] Test
-    # Naming syntax please use
-    # {Paper}-{Std/Norm}-{Win/''}-{Discr/''}-{epoch}-{train columns}
-    params = {'lr': 0.001, # learning rate
-                'loss': 'mean_absolute_percentage_error', # Loss function
-                'activation': 'tanh', # Not used
-                'recurrent_activation': 'sigmoid', # Not used
-                'epochs': 250, #250/500
-                'batch_size': 150,
-                'd': 22, # Taken from Roonwidala et al.
-                'train_columns': ['high', 'low', 'open', 'close'],
-                'label_column': 'close', 
-                'name': 'Pawar-Std-250-HighLowOpenClose', 
-                'discretization': False, # Use value rounding?
-                'fill_method': 'previous', # fillna method='pa'
-                'normalization': False, # Normalize or standardization?
-                'window_scaling': False } # Window scaling or bulk scaling?
-    
-    test = Test(Model=LSTM_PawarEtAl, params=params, tests=window_heavy_hitters_tests, f='', plot=True)
-    test.rolling_window_test('./forecasting_rnns/results/Pawar-Std-250-HighLowOpenClose/')
-
-    # ['open', 'close'] Test
-    # Naming syntax please use
-    # {Paper}-{Std/Norm}-{Win/''}-{Discr/''}-{epoch}-{train columns}
-    params = {'lr': 0.001, # learning rate
-                'loss': 'mean_absolute_percentage_error', # Loss function
-                'activation': 'tanh', # Not used
-                'recurrent_activation': 'sigmoid', # Not used
-                'epochs': 500, #250/500
-                'batch_size': 150,
-                'd': 22, # Taken from Roonwidala et al.
-                'train_columns': ['open', 'close'],
-                'label_column': 'close', 
-                'name': 'Pawar-Std-500-OpenClose', 
-                'discretization': False, # Use value rounding?
-                'fill_method': 'previous', # fillna method='pa'
-                'normalization': False, # Normalize or standardization?
-                'window_scaling': False } # Window scaling or bulk scaling?
-    
-    test = Test(Model=LSTM_PawarEtAl, params=params, tests=window_heavy_hitters_tests, f='', plot=True)
-    test.rolling_window_test('./forecasting_rnns/results/Pawar-Std-500-OpenClose/')
-
-    # ['high', 'low', 'close'] Test
-    # Naming syntax please use
-    # {Paper}-{Std/Norm}-{Win/''}-{Discr/''}-{epoch}-{train columns}
-    params = {'lr': 0.001, # learning rate
-                'loss': 'mean_absolute_percentage_error', # Loss function
-                'activation': 'tanh', # Not used
-                'recurrent_activation': 'sigmoid', # Not used
-                'epochs': 500, #250/500
-                'batch_size': 150,
-                'd': 22, # Taken from Roonwidala et al.
-                'train_columns': ['high', 'low', 'close'],
-                'label_column': 'close', 
-                'name': 'Pawar-Std-500-HighLowClose', 
-                'discretization': False, # Use value rounding?
-                'fill_method': 'previous', # fillna method='pa'
-                'normalization': False, # Normalize or standardization?
-                'window_scaling': False } # Window scaling or bulk scaling?
-    
-    test = Test(Model=LSTM_PawarEtAl, params=params, tests=window_heavy_hitters_tests, f='', plot=True)
-    test.rolling_window_test('./forecasting_rnns/results/Pawar-Std-500-HighLowClose/')
-
-    # ['high', 'low', 'open', 'close'] Test
-    # Naming syntax please use
-    # {Paper}-{Std/Norm}-{Win/''}-{Discr/''}-{epoch}-{train columns}
-    params = {'lr': 0.001, # learning rate
-                'loss': 'mean_absolute_percentage_error', # Loss function
-                'activation': 'tanh', # Not used
-                'recurrent_activation': 'sigmoid', # Not used
-                'epochs': 500, #250/500
-                'batch_size': 150,
-                'd': 22, # Taken from Roonwidala et al.
-                'train_columns': ['high', 'low', 'open', 'close'],
-                'label_column': 'close', 
-                'name': 'Pawar-Std-500-HighLowOpenClose', 
-                'discretization': False, # Use value rounding?
-                'fill_method': 'previous', # fillna method='pa'
-                'normalization': False, # Normalize or standardization?
-                'window_scaling': False } # Window scaling or bulk scaling?
-    
-    test = Test(Model=LSTM_PawarEtAl, params=params, tests=window_heavy_hitters_tests, f='', plot=True)
-    test.rolling_window_test('./forecasting_rnns/results/Pawar-Std-500-HighLowOpenClose/')
+    test.rolling_window_test('./forecasting_rnns/results/Pawar-Std-100-Close/')
